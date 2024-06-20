d724f1a191fb04811ae4786e69cb731c10cb51d8
wanny <choijw1116@gmail.com>
2024-06-20 16:23:08 +0900

    [Add] variables in BoardReadRes file

--- src/main/java/com/example/mojar_spring_project/board/model/BoardReadRes.java
+++ src/main/java/com/example/mojar_spring_project/board/model/BoardReadRes.java
@@ -3,13 +3,42 @@ package com.example.mojar_spring_project.board.model;
 public class BoardReadRes {
     String title;
     String content;
+    Integer max_capacity;
+    Integer post_type;
+    Integer user_idx;
 
     public BoardReadRes() {
     }
 
-    public BoardReadRes(String title, String content) {
+    public BoardReadRes(String title, String content, int post_type, int user_idx) {
         this.title = title;
         this.content = content;
+        this.post_type = post_type;
+        this.user_idx = user_idx;
+    }
+
+    public BoardReadRes(String title, String content, int max_capacity, int post_type, int user_idx) {
+        this.title = title;
+        this.content = content;
+        this.post_type = post_type;
+        this.user_idx = user_idx;
+        this.max_capacity = max_capacity;
+    }
+
+    public int getPost_type() {
+        return post_type;
+    }
+
+    public void setPost_type(int post_type) {
+        this.post_type = post_type;
+    }
+
+    public int getUser_idx() {
+        return user_idx;
+    }
+
+    public void setUser_idx(int user_idx) {
+        this.user_idx = user_idx;
     }
 
     public String getTitle() {
