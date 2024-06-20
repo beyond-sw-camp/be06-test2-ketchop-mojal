1f19943682b905bc4e2319d16e84c21e9a1104b6
shinebyul <qufdl8382@gmail.com>
2024-06-20 14:09:19 +0900

    [branch]dev에 초기파일

--- src/main/java/com/example/mojar_spring_project/member/MemberService.java
+++ src/main/java/com/example/mojar_spring_project/member/MemberService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberService {
     MemberRepository memberRepository;
-
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
