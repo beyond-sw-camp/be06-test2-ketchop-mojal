4efe18abb0b84a7db7437793f6052f3d953edbf6
wanny <choijw1116@gmail.com>
2024-06-20 16:22:24 +0900

    [Add] board read service

--- src/main/java/com/example/mojar_spring_project/board/BoardService.java
+++ src/main/java/com/example/mojar_spring_project/board/BoardService.java
@@ -1,12 +1,28 @@
 package com.example.mojar_spring_project.board;
-
+//HTTP요청응답처리하는곳!!!
+import com.example.mojar_spring_project.board.model.BoardReadRes;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoardService {
-    BoardService boardService;
+    BoardRepository boardRepository;
 
-    public BoardService(BoardService boardService) {
-        this.boardService = boardService;
+    //의존성주입
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
     }
+
+    //전체조회
+    public List<BoardReadRes> readAll(){
+      List<BoardReadRes> boardReadRes = boardRepository.findAll();
+      return boardReadRes;
+    };
+
+    //특정게시물조회
+    public List<BoardReadRes> read(Integer idx){
+        List<BoardReadRes> boardReadRes = boardRepository.findById(idx);
+        return boardReadRes;
+    };
 }
